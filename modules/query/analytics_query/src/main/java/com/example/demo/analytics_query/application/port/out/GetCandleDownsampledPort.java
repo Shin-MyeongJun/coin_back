58856d7d7b7f3fd6cd5db5fe3e5c70b7fb2e08dc
= <=>
2026-05-06 16:09:03 +0900

    controller 임시

--- modules/query/analytics_query/src/main/java/com/example/demo/analytics_query/application/port/out/GetCandleDownsampledPort.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.analytics_query.application.port.out;
-
-import com.example.demo.analytics_query.application.dto.CandleView;
-
-import java.util.List;
-
-public interface GetCandleDownsampledPort {
-    List<CandleView> findDownsampledTickCandles(Long marketCodeId, String sourceInterval, int targetBucketSeconds, Long fromTs, Long toTs);
-}
